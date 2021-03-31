b26f5b0d06c80c05b9e9b4581268fcadb36cf622
Romario Sarmiento <rsarmiento@rsarmiento-ltm.internal.salesforce.com>
2021-03-31 17:48:35 -0500

    .

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
+var repositoryRouter = require('./routes/repository');
 
 const cors = require("cors"),
       config = require("./config");
@@ -17,7 +18,7 @@ var app = express();
 // view engine setup
 app.use(cors(config.corsOptions));
 app.use(auth.session);
-app.use('/api/*', auth.required);
+//app.use('/api/*', auth.required);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/', repositoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const auth = require("../src/auth");
 
 router.post('/login', async (req, res, next) => {
-  console.log(auth.session);
   const email = req.body.email;
   const password = req.body.password;
   let account = {};
@@ -11,16 +9,13 @@ router.post('/login', async (req, res, next) => {
   const query = { email: email, password: password },
         options = {};
   const user = await userCollection.findOne(query, options);
-  console.log(user, '1');
   account = user;
   if(Object.keys(account).length > 0){
-    console.log(req.session, '2');
     req.session['user'] = {
       name: account.name,
       last_name: account.last_name,
-      email: account.email,
+      email: account.email
     };
-    console.log(req.session, '3');
     return res.status(200).json({success: true, data: account})
   }else{
     return res.status(200).json({success: false, data: []})
@@ -33,7 +28,6 @@ router.get('/logout', function (req, res) {
 });
 
 router.get('/session', function (req, res) {
-  console.log(req.session, '4');
   if ('user' in req.session){
       return res.status(200).json({success: true, data: req.session})
   }else{
